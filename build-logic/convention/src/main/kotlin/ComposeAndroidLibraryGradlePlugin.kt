bd3b832603c38cbc7b456e127851e19b8d0800e1
Abdullahi <abdulazeezabdullahi57@gmail.com>
2024-11-02 18:01:01 +0100

    create android library and compose library

---
+++ build-logic/convention/src/main/kotlin/ComposeAndroidLibraryGradlePlugin.kt
@@ -0,0 +1,64 @@
+import com.android.build.gradle.BaseExtension
+import com.example.repofinder.AndroidConfig
+import com.example.repofinder.GradlePluginsConfig.ANDROID_LIBRARY
+import org.gradle.api.JavaVersion
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.getByType
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+class ComposeAndroidLibraryGradlePlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        target.addComposeLibraryPlugin()
+        target.addComposeLibraryBlock()
+    }
+}
+
+internal fun Project.addComposeLibraryPlugin() {
+    plugins.apply(ANDROID_LIBRARY)
+}
+
+internal fun Project.addComposeLibraryBlock() = this.extensions.getByType<BaseExtension>().run {
+    compileSdkVersion(AndroidConfig.COMPILE_SDK_VERSION)
+    defaultConfig {
+
+        minSdk = AndroidConfig.MIN_SDK_VERSION
+        targetSdk = AndroidConfig.TARGET_SDK_VERSION
+        versionCode = AndroidConfig.VERSION_CODE
+        versionName = AndroidConfig.VERSION_NAME
+        testInstrumentationRunner = AndroidConfig.TEST_INSTRUMENTATION_RUNNER
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+
+
+    project.tasks.withType(KotlinCompile::class.java).configureEach {
+        kotlinOptions {
+            jvmTarget = JavaVersion.VERSION_21.toString()
+            freeCompilerArgs = listOf(
+                "-Xstring-concat=inline"
+            )
+        }
+    }
+
+    buildFeatures.buildConfig = true
+
+    //Compose Features
+    buildFeatures.apply {
+        compose = true
+    }
+
+    composeOptions.apply {
+        kotlinCompilerExtensionVersion = "1.5.7"
+    }
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+
+}
