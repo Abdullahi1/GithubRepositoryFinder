87d25e4907203a053af8e18ced91895fbbd244b9
Abdullahi <abdulazeezabdullahi57@gmail.com>
2024-11-03 18:58:08 +0100

    move color scheme to common design and code refactor

--- core/commondesign/src/main/java/com/example/commondesign/component/theme/Theme.kt
+++ core/commondesign/src/main/java/com/example/commondesign/component/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.example.repofinder.ui.theme
+package com.example.commondesign.component.theme
 
 import android.app.Activity
 import android.os.Build
--- core/commondesign/src/main/java/com/example/commondesign/component/theme/Type.kt
+++ core/commondesign/src/main/java/com/example/commondesign/component/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.example.repofinder.ui.theme
+package com.example.commondesign.component.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
