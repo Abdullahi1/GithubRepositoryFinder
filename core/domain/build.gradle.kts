8204a5a07086cbc9281821fa37fd36d8d2f088de
Abdullahi <abdulazeezabdullahi57@gmail.com>
2024-11-03 13:49:16 +0100

    fix module dependency

--- core/domain/build.gradle.kts
+++ core/domain/build.gradle.kts
@@ -3,3 +3,14 @@ plugins {
     alias(libs.plugins.repofinder.jvm.library)
 //    alias(libs.plugins.jetbrains.kotlin.jvm)
 }
+
+dependencies {
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.javax.inject)
+
+    testImplementation(libs.junit)
+    testImplementation(libs.truth)
+    testImplementation(libs.mockito)
+    testImplementation(libs.mockito.kotlin)
+    testImplementation(libs.kotlinx.coroutines.test)
+}
--- feature/home/build.gradle.kts
+++ feature/home/build.gradle.kts
@@ -11,6 +11,18 @@ dependencies {
 
     implementation(projects.core.domain)
 
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+
+    implementation(libs.coil.kt)
+    implementation(libs.coil.kt.compose)
+    implementation(libs.coil.kt.svg)
+    implementation(libs.kotlinx.serialization.json)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
