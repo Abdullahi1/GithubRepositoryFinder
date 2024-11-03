755bcce555700fd64a0dfda3bfedd7625388bc6d
Abdullahi <abdulazeezabdullahi57@gmail.com>
2024-11-03 18:58:08 +0100

    move color scheme to common design and code refactor

--- app/src/main/java/com/example/repofinder/MainActivity.kt
+++ app/src/main/java/com/example/repofinder/MainActivity.kt
@@ -11,7 +11,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.repofinder.ui.theme.GithubRepositoryFinderTheme
+import com.example.commondesign.component.theme.GithubRepositoryFinderTheme
+import com.example.home.HomeSample
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
