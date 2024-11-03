802c78889ff37bcd080a86116db393e3702a7ed4
Abdullahi <abdulazeezabdullahi57@gmail.com>
2024-11-03 13:49:16 +0100

    fix module dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("dagger.hilt.android.plugin")
 }
 
 android {
@@ -27,6 +28,15 @@ dependencies {
 
     implementation(projects.core.domain)
     implementation(projects.core.data)
+
+    implementation(libs.androidx.core.splashscreen)
+    implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.androidx.lifecycle.runtimeCompose)
+    implementation(libs.androidx.navigation.compose)
+
+
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -12,10 +12,30 @@ dependencies {
 
     implementation(projects.core.domain)
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.logging)
+    implementation(libs.retrofit.core)
+    implementation(libs.moshi)
+    implementation(libs.retrofit.coroutine.adapter)
+    implementation(libs.retrofit.moshi.converter)
+
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.javax.inject)
+    implementation(libs.hilt.android)
+
+    ksp(libs.hilt.compiler)
+    ksp(libs.moshi.codegen)
+
     testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    testImplementation(libs.mockito)
+    testImplementation(libs.mockito.kotlin)
+    testImplementation(libs.mock.web.server)
+    testImplementation(libs.kotlinx.coroutines.test)
+
+//    implementation(libs.androidx.core.ktx)
+//    implementation(libs.androidx.appcompat)
+//    implementation(libs.material)
+//
+//    androidTestImplementation(libs.androidx.junit)
+//    androidTestImplementation(libs.androidx.espresso.core)
 }
