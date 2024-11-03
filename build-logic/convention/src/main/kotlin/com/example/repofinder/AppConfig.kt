e1e0aef25d7dea2bbd0e4b5072618170eced0370
Abdullahi <abdulazeezabdullahi57@gmail.com>
2024-11-03 13:09:11 +0100

    increase gradle version

--- build-logic/convention/src/main/kotlin/com/example/repofinder/AppConfig.kt
+++ build-logic/convention/src/main/kotlin/com/example/repofinder/AppConfig.kt
@@ -4,9 +4,9 @@ import org.gradle.api.JavaVersion
 
 object AndroidConfig {
     const val APP_NAME = "Github Repository Finder"
-    const val COMPILE_SDK_VERSION = 34
+    const val COMPILE_SDK_VERSION = 35
     const val MIN_SDK_VERSION = 24
-    const val TARGET_SDK_VERSION = 34
+    const val TARGET_SDK_VERSION = 35
     const val VERSION_CODE = 1
     const val VERSION_NAME = "1.0.0"
     const val APPLICATION_ID = "com.example.repofinder"
