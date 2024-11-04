aa95fa6e1c5b6cc1196e286af514d0aeed61d14a
Abdullahi <abdulazeezabdullahi57@gmail.com>
2024-11-04 06:11:40 +0100

    package refactor

--- feature/home/src/main/java/com/example/home/component/HomeMenuCardItem.kt
+++ feature/home/src/main/java/com/example/home/component/HomeMenuCardItem.kt
@@ -24,7 +24,7 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.commondesign.component.theme.GithubRepositoryFinderTheme
+import com.example.commondesign.theme.GithubRepositoryFinderTheme
 
 @Composable
 fun HomeMenuCardItem(
