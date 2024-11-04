3fdc576a2989817053b974e8547b0d3ed4c271a9
Abdullahi <abdulazeezabdullahi57@gmail.com>
2024-11-04 13:20:14 +0100

    implement user list component

---
+++ core/domain/src/main/java/com/example/domain/model/GithubUserData.kt
@@ -0,0 +1,10 @@
+package com.example.domain.model
+
+data class GithubUserData(
+    val userName: String,
+    val fullName: String,
+    val bio: String,
+    val location: String,
+    val emailAddress: String,
+    val imageUrl: String
+)
