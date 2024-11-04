9b9355e021d4077371bbe9fa5f2746d1a4c78dc5
Abdullahi <abdulazeezabdullahi57@gmail.com>
2024-11-04 13:38:29 +0100

    create userdetails module

---
+++ feature/userdetails/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+//    alias(libs.plugins.android.library)
+    alias(libs.plugins.repofinder.android.compose.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.example.userdetails"
+}
+
+dependencies {
+
+    implementation(projects.core.domain)
+    implementation(projects.core.commondesign)
+
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    debugImplementation(libs.androidx.ui.tooling)
+
+    implementation(libs.coil.kt)
+    implementation(libs.coil.kt.compose)
+    implementation(libs.coil.kt.svg)
+    implementation(libs.kotlinx.serialization.json)
+
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
