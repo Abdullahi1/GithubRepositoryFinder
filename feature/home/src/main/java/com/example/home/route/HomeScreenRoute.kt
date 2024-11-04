00c72b292810ff2067463ece665376b36f3a44d8
Abdullahi <abdulazeezabdullahi57@gmail.com>
2024-11-04 17:12:58 +0100

    add route

---
+++ feature/home/src/main/java/com/example/home/route/HomeScreenRoute.kt
@@ -0,0 +1,6 @@
+package com.example.home.route
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+object HomeScreenRoute
---
+++ feature/repositories/src/main/java/com/example/repositories/route/RepositoryScreenRoute.kt
@@ -0,0 +1,6 @@
+package com.example.repositories.route
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+object RepositoryScreenRoute
