a238f3950615056536f267645c04abd6dcf50cfb
Abdullahi <abdulazeezabdullahi57@gmail.com>
2024-11-05 09:00:15 +0100

    create test cases fot GithubRepoRepositories

---
+++ core/data/src/test/java/com/example/data/common/FakeResponseData.kt
@@ -0,0 +1,7 @@
+package com.example.data.common
+
+import com.example.data.model.repository.SearchRepositoryListResponse
+
+object FakeResponseData {
+    val fakeGithubRepoSearchResponse = SearchRepositoryListResponse(100, false)
+}
