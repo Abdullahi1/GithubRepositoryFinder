b3913d2bb49e2904521bac3cd8d51ebf3e3f6698
Abdullahi <abdulazeezabdullahi57@gmail.com>
2024-11-05 13:16:29 +0100

    create get github user repository usecase with test case

--- core/domain/src/test/java/com/example/domain/common/FakeDataClass.kt
+++ core/domain/src/test/java/com/example/domain/common/FakeDataClass.kt
@@ -3,6 +3,7 @@ package com.example.domain.common
 import com.example.domain.model.GithubRepositoryData
 import com.example.domain.model.GithubSearchUserData
 import com.example.domain.model.GithubUserProfileData
+import com.example.domain.model.GithubUserRepositoryData
 
 object FakeDataClass {
     val repositoryData = GithubRepositoryData(
@@ -38,5 +39,26 @@ object FakeDataClass {
         bio = "These are random words that will be replaced in due time. ",
         location = "Lagos, Nigeria",
         emailAddress = ""
+    )
+
+    val githubUserRepositoryData = GithubUserRepositoryData(
+        repositoryId = 100,
+        fullName = "Vundere/Python",
+        githubUrl = "",
+        topics = listOf(
+            "Design system",
+            "Component- misc",
+            "Status- New",
+            "Html",
+            "Css",
+            "JS",
+            "Html",
+            "Css",
+            "JS"
+        ),
+        language = "Python",
+        repositoryDescription = "These are random words that will be replaced in due time. Config files for my github profile",
+        watchersCount = 10,
+        userUrl = ""
     )
 }
