310389ee3d153b4363d9a521fc24d601a5e3f7da
Abdullahi <abdulazeezabdullahi57@gmail.com>
2024-11-06 13:24:31 +0100

    implement github user details viewmodel with test case

---
+++ feature/userdetails/src/test/java/com/example/userdetails/common/FakeData.kt
@@ -0,0 +1,15 @@
+package com.example.userdetails.common
+
+import com.example.domain.model.GithubUserProfileData
+
+object FakeData {
+
+    val userProfile = GithubUserProfileData(
+        userName = "",
+        fullName = "",
+        bio = "",
+        location = "",
+        emailAddress = "",
+        imageUrl = ""
+    )
+}
