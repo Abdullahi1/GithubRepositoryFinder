6c64d55004814b05a17bfc209e57b98befa999b9
Abdullahi <abdulazeezabdullahi57@gmail.com>
2024-11-07 04:03:21 +0100

    add font to project

--- core/commondesign/src/main/java/com/example/commondesign/theme/Type.kt
+++ core/commondesign/src/main/java/com/example/commondesign/theme/Type.kt
@@ -2,12 +2,191 @@ package com.example.commondesign.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.text.style.LineHeightStyle.Alignment
+import androidx.compose.ui.text.style.LineHeightStyle.Trim
 import androidx.compose.ui.unit.sp
+import com.example.commondesign.R
+
+val ManRopeFont = FontFamily(
+    Font(
+        resId = R.font.manrope_extra_light, weight = FontWeight.W200,
+        style = FontStyle.Normal
+    ),
+    Font(
+        resId = R.font.manrope_light, weight = FontWeight.W300,
+        style = FontStyle.Normal
+    ),
+    Font(
+        resId = R.font.manrope_regular, weight = FontWeight.W400,
+        style = FontStyle.Normal
+    ),
+
+    Font(
+        resId = R.font.manrope_medium, weight = FontWeight.W500,
+        style = FontStyle.Normal
+    ),
+    Font(
+        resId = R.font.manrope_semi_bold, weight = FontWeight.W600,
+        style = FontStyle.Normal
+    ),
+    Font(
+        resId = R.font.manrope_bold, weight = FontWeight.W700,
+        style = FontStyle.Normal
+    ),
+    Font(
+        resId = R.font.manrope_extra_bold, weight = FontWeight.W800,
+        style = FontStyle.Normal
+    ),
+    Font(
+        resId = R.font.manrope_extra_bold, weight = FontWeight.W900,
+        style = FontStyle.Normal
+    )
+)
+
+internal val Typography = Typography(
+    displayLarge = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 57.sp,
+        lineHeight = 64.sp,
+        letterSpacing = (-0.25).sp,
+        fontFamily = ManRopeFont
+    ),
+    displayMedium = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 45.sp,
+        lineHeight = 52.sp,
+        letterSpacing = 0.sp,
+        fontFamily = ManRopeFont
+    ),
+    displaySmall = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 36.sp,
+        lineHeight = 44.sp,
+        letterSpacing = 0.sp,
+        fontFamily = ManRopeFont
+    ),
+    headlineLarge = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 32.sp,
+        lineHeight = 40.sp,
+        letterSpacing = 0.sp,
+        fontFamily = ManRopeFont
+    ),
+    headlineMedium = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 28.sp,
+        lineHeight = 36.sp,
+        letterSpacing = 0.sp,
+        fontFamily = ManRopeFont
+    ),
+    headlineSmall = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp,
+        lineHeight = 32.sp,
+        letterSpacing = 0.sp,
+        lineHeightStyle = LineHeightStyle(
+            alignment = Alignment.Bottom,
+            trim = Trim.None,
+        ),
+        fontFamily = ManRopeFont
+    ),
+    titleLarge = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp,
+        lineHeightStyle = LineHeightStyle(
+            alignment = Alignment.Bottom,
+            trim = Trim.LastLineBottom,
+        ),
+        fontFamily = ManRopeFont
+    ),
+    titleMedium = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.1.sp,
+        fontFamily = ManRopeFont
+    ),
+    titleSmall = TextStyle(
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.1.sp,
+        fontFamily = ManRopeFont
+    ),
+    // Default text style
+    bodyLarge = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp,
+        lineHeightStyle = LineHeightStyle(
+            alignment = Alignment.Center,
+            trim = Trim.None,
+        ),
+        fontFamily = ManRopeFont
+    ),
+    bodyMedium = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.25.sp,
+        fontFamily = ManRopeFont
+    ),
+    bodySmall = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.4.sp,
+        fontFamily = ManRopeFont
+    ),
+    // Used for Button
+    labelLarge = TextStyle(
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.1.sp,
+        lineHeightStyle = LineHeightStyle(
+            alignment = Alignment.Center,
+            trim = Trim.LastLineBottom,
+        ),
+        fontFamily = ManRopeFont
+    ),
+    // Used for Navigation items
+    labelMedium = TextStyle(
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp,
+        lineHeightStyle = LineHeightStyle(
+            alignment = Alignment.Center,
+            trim = Trim.LastLineBottom,
+        ),
+        fontFamily = ManRopeFont
+    ),
+    // Used for Tag
+    labelSmall = TextStyle(
+        fontWeight = FontWeight.Medium,
+        fontSize = 10.sp,
+        lineHeight = 14.sp,
+        letterSpacing = 0.sp,
+        lineHeightStyle = LineHeightStyle(
+            alignment = Alignment.Center,
+            trim = Trim.LastLineBottom,
+        ),
+        fontFamily = ManRopeFont
+    ),
+)
+
 
 // Set of Material typography styles to start with
-val Typography = Typography(
+val AppTypography = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
